aa39217bcafd7521a2e28b5282949309fa56252d
unknown <nguyenminhtri08111997@gmail.com>
2022-01-04 09:34:43 +0700

    casestudy

---
+++ index.js
@@ -0,0 +1,12 @@
+function checkAccount(){
+    let acc = document.getElementById("account").value
+    let pass = document.getElementById("pass").value
+    if(acc === "admin" && pass === "123456" ){
+        alert('Chào mừng bạn đến với trang thông tin Codegym')
+        // document.getElementById("dangNhap").action = "index.html"
+        window.location.href = 'form-login/login.html'
+    }else {
+        alert('Sai thông tin đăng nhập')
+        document.getElementById("dangNhap").action = "login.html"
+    }
+}
