a7b3c03a0ea3372c29acd72cbdbb89f519b0ea61
unknown <nguyenminhtri08111997@gmail.com>
2022-01-05 15:53:48 +0700

    casestudy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function checkAccount(){
     let acc = document.getElementById("account").value
     let pass = document.getElementById("pass").value
     if(acc === "admin" && pass === "123456" ){
-        alert('Chào mừng bạn đến với trang thông tin Codegym')
+        alert('Chào mừng bạn đến với trang baomoi.net')
         // document.getElementById("dangNhap").action = "index.html"
         window.location.href = 'form-login/login.html'
     }else {
